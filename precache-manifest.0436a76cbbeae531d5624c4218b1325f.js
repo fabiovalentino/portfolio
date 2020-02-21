a75f0216fe7788beddd4d9b6c7441683aea7abd9
blast <rodfval@gmail.com>
2020-02-21 14:56:17 +0100

    Updates

---
+++ precache-manifest.0436a76cbbeae531d5624c4218b1325f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75fa4fc4c0636dec5084b8619f0ea966",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "550ebe1e66cbf4473c34",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8775563f26236cae13d4",
+    "url": "/portfolio/static/js/2.aabefd6c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.aabefd6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "550ebe1e66cbf4473c34",
+    "url": "/portfolio/static/js/main.2194ff4d.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
